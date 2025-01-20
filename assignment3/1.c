84464d3693563e337dbfccb912497ac6de6e720c
Preethamchegu <chegupreetham26@gmail.com>
2025-01-20 15:24:30 +0530

    1.c

--- assignment3/1.c
+++ assignment3/1.c
@@ -1,30 +1,39 @@
 #include<stdio.h>
+#include<stdlib.h> 
 
-struct linked{
-	int data;
-	struct linked *next;
+struct linked {
+    int data;
+    struct linked *next;
 };
-void insertatbegin(struct linked **head,int num )
-{
-	struct linked *new = (struct linked*) malloc(sizeof(struct linked));
-	new->data=num;
-	new->next=null;
-	if(*head==NULL)
-	{
-		*head=new;		
-	}
-	else
-	{
-		new->next=*head;
-		*head=new;
-	}
+
+void insertatbegin(struct linked **head, int num) {
+    struct linked *new = (struct linked *) malloc(sizeof(struct linked));
+    if (new == NULL) {
+        printf("Memory allocation failed\n");
+        return;
+    }
+    new->data = num;
+    new->next = NULL;
+    if (*head == NULL) {
+        *head = new;        
+    } else {
+        new->next = *head;
+        *head = new;
+    }
 }
-int main()
-{   
-	struct linked *head=null; 
-	insertatbegin(&head,10);
-	insertatbegin(&head,20);
-	insertatbegin(&head,30);
-	insertatbegin(&head,40);
-	
+
+
+
+
+int main() {
+    struct linked *head = NULL; // Initialize head to NULL
+    insertatbegin(&head, 10);
+    insertatbegin(&head, 20);
+    insertatbegin(&head, 30);
+    insertatbegin(&head, 40);
+
+    
+    printList(head);
+
+    return 0;
 }
