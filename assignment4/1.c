c61d3941f2bcafe55671a77652bedecc768c5db8
Preethamchegu <chegupreetham26@gmail.com>
2025-01-27 15:09:02 +0530

    1.c

---
+++ assignment4/1.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{
+	int data;
+	struct node *next;
+} node;
+int main()
+{	int n,k,i,num;
+	printf("enter no.of nodes:");
+	scanf("%d",&n);
+	printf("enter numbers from last: ");
+	node *head=NULL;
+	for(i=0;i<n;i++){
+		node *new=(node*)malloc(sizeof(node));
+		scanf("%d",&num);
+		if(head==NULL){
+			new->data=num;
+			head=new;
+			new->next=NULL;
+		}
+		else{
+			new->data=num;
+			new->next=head;
+			head=new;	
+		}
+	}
+	printf("before rotations your list in form");
+	node *temp=head;
+	while(temp!=NULL)
+	{
+		printf("%d ->",temp->data);
+		temp=temp->next;	
+	}
+	printf("\n enter no. of rotations:");
+	scanf("%d",&k);
+	node *last=head;
+	node *first=head;
+	for(i=0;i<k;i++){
+		while( last!=NULL){
+			if(last->next==NULL)
+			{
+				last->next=first;
+				head=head->next;
+				first->next=NULL;
+				first=head;
+				last=head;
+				break;
+			}
+			else{
+				last=last->next;
+			}
+		}
+		
+	}
+	printf("after rotating:\n");
+	node *line=head;
+	while(line!=NULL)
+	{
+		printf("%d->",line->data);
+		line=line->next;	
+	}
+	temp=head;
+	while(temp!=NULL){
+	    head=head->next;
+	    free(temp);
+	    temp=head;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
