2e597c2c813d892dcd0a12803229c6aee31f6b07
Preethamchegu <chegupreetham26@gmail.com>
2025-02-03 00:46:03 +0530

    1.c

--- assignment4/1.c
+++ assignment4/1.c
@@ -4,81 +4,75 @@ typedef struct node{
 	int data;
 	struct node *next;
 } node;
-
-int main() 
-{   int n,i,num;
-    printf("enter no.of nodes:");
+int main()
+{	int n,k,i,num;
+	printf("enter no.of nodes:");
 	scanf("%d",&n);
-	node *prev;
+	printf("enter numbers from last: ");
+	node *head=NULL;
 	for(i=0;i<n;i++){
 		node *new=(node*)malloc(sizeof(node));
 		scanf("%d",&num);
-		if(head == NULL){
+		if(head==NULL){
 			new->data=num;
 			head=new;
 			new->next=NULL;
-            prev=new;
 		}
 		else{
 			new->data=num;
-			new->next=NULL;
-			prev->next=new; 
-            prev=prev->next;	
+			new->next=head;
+			head=new;	
 		}
 	}
-    printf("before removal:\n");
-    node *temp=head;
-        while(temp!=NULL)
-        {
-            printf("%d ->",temp->data);
-            temp=temp->next;	
-        }
-        printf("NULL");
-        node *move;
-        node *move_previous;
-        node *check=head;
-        while(check!= NULL && check->next!=NULL){
-            
-                move_previous=check;
-                move=check->next;
-            
-            while( move!=NULL){
-                if(check->data==move->data)
-                {
-                    move_previous->next=move->next;
-                    free(move);
-                    move=move_previous->next;
-                    
-                }
-                else{
-                    move_previous=move;
-                    move=move->next;
-                }
-            }
-            check=check->next;	
-        }
-        printf("\nafter removal:\n");
-        node *line=head;
-        while(line!=NULL)
-        {
-            printf("%d->",line->data);
-            line=line->next;	
-        }
-        printf("NULL");
-        temp=head;
-        while(temp!=NULL){
-            head=head->next;
-            free(temp);
-            temp=head;
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+	printf("before rotations your list in form");
+	node *temp=head;
+	while(temp!=NULL)
+	{
+		printf("%d ->",temp->data);
+		temp=temp->next;	
+	}
+	printf("\n enter no. of rotations:");
+	scanf("%d",&k);
+	node *last=head;
+	node *first=head;
+	for(i=0;i<k;i++){
+		while( last!=NULL){
+			if(last->next==NULL)
+			{
+				last->next=first;
+				head=head->next;
+				first->next=NULL;
+				first=head;
+				last=head;
+				break;
+			}
+			else{
+				last=last->next;
+			}
+		}
+		
+	}
+	printf("after rotating:\n");
+	node *line=head;
+	while(line!=NULL)
+	{
+		printf("%d->",line->data);
+		line=line->next;	
+	}
+	temp=head;
+	while(temp!=NULL){
+	    head=head->next;
+	    free(temp);
+	    temp=head;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
